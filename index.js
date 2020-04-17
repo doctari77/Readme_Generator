7d47f8bd3a3bf27860b6f8f60b6b5bc03f1ff103
Todd Leslie <spotontechnology@gmail.com>
2020-04-16 21:03:53 -0600

    updates after tutor1

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
 const util = require("util");
-const apiCall = require('./utils/api.js')
+const apiCall = require('./utils/api.js');
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
@@ -69,30 +69,7 @@ const writeFileAsync = util.promisify(fs.writeFile);
     
 
 function generateMarkDown (answers) {
-  /*return `
-  
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-    <p class="lead">I am from ${answers.location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${answers.github}</li>
-      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;*/
+ 
 }
 function writeToFile (filename, data){
   return fs.writeFileSync(path.join (process.cwd(),filename),data)
@@ -104,16 +81,6 @@ function writeToFile (filename, data){
      })
    })
   
-  /*try {
-    const answers = await promptUser();
-
-    const markDown = generatemarkDown(answers);
-
-    await writeFileAsync("readMe.md", Text);
-
-    
-    console.log(err);
-  }
-*/
+  
 }
 init();
